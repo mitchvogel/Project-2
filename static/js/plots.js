88bd1796554083501b8b2078c282230578d11a07
John Gotz <gotzx015@d.umn.edu>
2021-07-24 14:21:06 -0500

    Updated plots.js

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,6 +1,61 @@
+var state_combined_url = "/api/statecombineddata";
+var state_url = "/api/statedata";
 var city_url = "/api/citydata";
-//var state_url = 
 
-d3.json(city_url).then(function(data) {
-   console.log(data);
+d3.json(state_combined_url).then(function(data) {
+   //console.log(data);
+
+   state_names = [];
+   avg = [];
+
+   for (var i=0; i<data.length; i++) {
+
+      state_names.push(data[i].state);
+      avg.push(data[i].agg_median_price);
+
+   }
+   
+   const labels = state_names;
+   const scatter_data = {
+     labels: labels,
+     datasets: [{
+       label: 'Aggregate Median Household Prices',
+       data: avg,
+       showLine: false,
+       pointBackgroundColor: 'black',
+       pointRadius: 5
+     }]
+   };
+
+   var ctx = document.getElementById('scatterChart').getContext('2d');
+
+   var scatterChart = new Chart(ctx, {
+      type: 'line',
+      data: scatter_data,
+      options: {
+         scales: {
+            yAxes: [{
+               ticks: {
+                  fontColor: 'black'
+               }
+            }],
+            xAxes: [{
+               ticks: {
+                  fontColor: 'black'
+               }
+            }]
+         }
+         
+   
+      
+      }
+   });
 });
+
+// d3.json(state_url).then(function(data) {
+//    console.log(data);
+// });
+
+// d3.json(city_url).then(function(data) {
+//    console.log(data);
+// });
